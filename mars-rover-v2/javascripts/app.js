ed44e59b2c94cee3c76705489e38b58449973347
Ware Carson <20378@columbushs.com>
2019-11-12 10:26:28 -0500

    firstcommit

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,15 +1,57 @@
 // Rover Object Goes Here
+let rover ; {
+  direction: "N";
+}
+
 // ======================
 
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+
+  if (rover.direction == "N"){
+    rover.direction = "W";
+  }
+  else if (rover.direction == "W"){
+    rover.direction ="S";
+  }
+  else if (rover.direction == "N"){
+    rover.direction ="E";
+  }
+else if {
+rover.direction = "N";
 }
+
+
+}
+
 
 function turnRight(rover){
   console.log("turnRight was called!");
 }
+if (rover.direction == "N"){
+  rover.direction = "E";
+}
+else if (rover.direction == "E"){
+  rover.direction = "S";
+}
+else if (rover.direction == "S"){
+  rover.direction = "W";
+}
+else{
+  rover.direction = "N";
+}
+
 
 function moveForward(rover){
   console.log("moveForward was called")
+}
+if (rover.direction == "N"){
+  rover.direction = "N";
+}
+else if (rover.direction == "W"){
+  rover.direction = "W";
+else {
+  rover.direction = "E";
+   }
 }
